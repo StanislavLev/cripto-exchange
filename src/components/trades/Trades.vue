cb82396b286599f62e86a663d9ac2965c40d003b
Stas <pschnk@gmail.com>
2018-09-07 16:56:14 -0700

    add 2s interval for fetchTrades requests

--- src/components/trades/Trades.vue
+++ src/components/trades/Trades.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>TRADES</h2>
+    <h2>TRADES : {{dateNow.getUTCHours()}} : {{dateNow.getUTCMinutes()}} : {{dateNow.getUTCSeconds()}}</h2>
     <h4 v-show="errMsg">{{errMsg}}</h4>
     <h4 v-for="trade in allTrades">{{ trade.amount }} : {{ trade.price }} : {{ trade.datetime }}<br /> </h4>
   </div>
@@ -14,11 +14,13 @@ export default {
   name: 'Trades',
   data() {
     return {
+      dateNow: new Date(),
       errMsg: '',
       exchange: {},
       pair: '',
       allTrades: [],
       limit: 20,
+      fetchTradesInterval: undefined,
     };
   },
   created(){
@@ -28,18 +30,32 @@ export default {
       this.exchange = exchange;
       this.pair = pair;
       let since = undefined;
-      (async () => {
-        if (this.exchange.has['fetchTrades']) {
-          try {
-            this.allTrades = await exchange.fetchTrades (this.pair, since, this.limit);
-          } catch(err) {
-            this.errMsg = "Failed to fetch the trades info, please try another pair or exchange."
+      if(this.fetchTradesInterval){
+        clearInterval(this.fetchTradesInterval);
+      }
+      this.fetchTradesInterval = setInterval(
+        async () => {
+          if (this.exchange.has['fetchTrades']) {
+            try {
+              this.allTrades = await exchange.fetchTrades (this.pair, since, this.limit);
+              this.dateNow = new Date();
+            } catch(err) {
+              this.errMsg = "Failed to fetch the trades info, please try another pair or exchange."
+              this.allTrades = [];
+              if(this.fetchTradesInterval){
+                clearInterval(this.fetchTradesInterval);
+              }
+            }
           }
-        }
-      })();
+        }, 
+      2000);
     });
     bus.$on('exchangeChosenErr', (err)=>{
       this.allTrades = [];
+      this.errMsg = '';
+      if(this.fetchTradesInterval){
+        clearInterval(this.fetchTradesInterval);
+      }
     });
   },
 };
